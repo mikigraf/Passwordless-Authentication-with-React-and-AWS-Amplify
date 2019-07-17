56e4cd1265c1633739ecf009d4da4f73cf80982d
Miki Wawrzyniak <mikolajspejs@gmail.com>
2019-07-17 20:49:51 +0200

    re

--- infrastructure/lambda-triggers/02-create-auth-challenge/create-auth-challenge.js
+++ infrastructure/lambda-triggers/02-create-auth-challenge/create-auth-challenge.js
@@ -1,10 +1,28 @@
 
 const mongoose = require('mongoose');
 
-module.exports.handler = async event => {
+let conn = null;
+
+module.exports.handler = async (event, context) => {
     const connectionString = process.env.DB_CONNECTION_STRING
 
-    mongoose.connect(connectionString);
+    // Make sure to add this so you can re-use `conn` between function calls.
+    // See https://www.mongodb.com/blog/post/serverless-development-with-nodejs-aws-lambda-mongodb-atlas
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    // Because `conn` is in the global scope, Lambda may retain it between
+    // function calls thanks to `callbackWaitsForEmptyEventLoop`.
+    // This means your Lambda function doesn't have to go through the
+    // potentially expensive process of connecting to MongoDB every time.
+    if (conn == null) {
+        conn = await mongoose.createConnection(connectionString, {
+        // Buffering means mongoose will queue up operations if it gets
+        // disconnected from MongoDB and send them when it reconnects.
+        // With serverless, better to fail fast if not connected.
+        bufferCommands: false, // Disable mongoose buffering
+        bufferMaxEntries: 0 // and MongoDB driver buffering
+        });
+    }
 
     const { Schema } = mongoose;
     const userSchema = new Schema({
@@ -37,7 +55,7 @@ module.exports.handler = async event => {
     }
 
     // This is sent back to the client app
-    event.response.publicChallengeParameters = { username: event.request.userAttributes.username };
+    event.response.publicChallengeParameters = { username: event.request.userAttributes.email };
 
     // Add the secret login code to the private challenge parameters
     // so it can be verified by the "Verify Auth Challenge Response" trigger
