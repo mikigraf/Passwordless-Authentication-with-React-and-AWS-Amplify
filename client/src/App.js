62543d404162a00a02dcfe57bd8fb222dcb38632
Mikołaj Wawrzyniak <spejss@users.noreply.github.com>
2019-07-17 19:22:28 +0200

    asdasdasd

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react';
+import { Auth } from 'aws-amplify';
+import { Form, Button, Input, Notification, Radio, Progress } from "element-react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const state = {
+    email: ""
+  };
+
+  render(){
+    return (
+      <div className="App">
+        { /* login */ }
+        <div>
+          <Form className="login-form">
+            <Form.Item label="email">
+              <Input type="text" icon="user" placeholder="Email" onChange={email => this.setState({email})} />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" disabled={!email} onClick={this.handleEmailInput}>Sign In</Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify'
+import aws_exports from './aws-exports'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
